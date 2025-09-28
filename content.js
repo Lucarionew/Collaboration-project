0bc289e323d9a3556a381e5a7ef9ea96fa7a9518
SanjanaGadapa <sanjugadapa@gmail.com>
2025-09-28 20:41:21 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,39 +1,20 @@
-import { db } from "./firebase-config.js";
-import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
-
+// In content.js, just send messages
 sendLoginEvent() {
   const email = this.extractEmail();
-  const data = {
+  chrome.runtime.sendMessage({
+    type: 'LOGIN',
     hostname: this.hostname,
     url: this.url,
     email: email,
     timestamp: Date.now()
-  };
-  console.log("SessionSync: Sending LOGIN event", data);
-
-  chrome.runtime.sendMessage({ type: 'LOGIN', ...data });
-
-  // Optional: Direct Firestore sync from content script
-  if (db) {
-    addDoc(collection(db, "sessions"), { ...data, device: navigator.userAgent, timestamp: serverTimestamp() })
-      .then(() => console.log('SessionSync: LOGIN saved from content script'))
-      .catch(err => console.error('SessionSync: Firestore LOGIN error', err));
-  }
+  });
 }
 
 sendLogoutEvent() {
-  const data = {
+  chrome.runtime.sendMessage({
+    type: 'LOGOUT',
     hostname: this.hostname,
     url: this.url,
     timestamp: Date.now()
-  };
-  console.log("SessionSync: Sending LOGOUT event", data);
-
-  chrome.runtime.sendMessage({ type: 'LOGOUT', ...data });
-
-  if (db) {
-    addDoc(collection(db, "sessions"), { ...data, device: navigator.userAgent, active: false, timestamp: serverTimestamp() })
-      .then(() => console.log('SessionSync: LOGOUT saved from content script'))
-      .catch(err => console.error('SessionSync: Firestore LOGOUT error', err));
-  }
+  });
 }
